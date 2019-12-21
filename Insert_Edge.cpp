0756f0ae47a26869299e3b335e48e9ba09c65d24
Supermonky <1450126348@qq.com>
2019-12-21 18:52:29 +0800

    小修改

---
+++ Insert_Edge.cpp
@@ -0,0 +1,31 @@
+#include"insert_edge.h"
+bool Graphcnt::insertEdge(int v1, int v2, int cost, string v3)
+{
+
+	if (v1 >= 0 && v1 <= numnode && v2 >= 0 && v2 <= numnode)//v1,v2合法 
+	{
+		string a;
+		int b, c;
+		Edge* q, * p;
+		p = NodeTable[v1].adj;
+		while (p != NULL && p->dest != NodeTable[v2].start)
+			p = p->link;
+		if (p != NULL)
+		{
+			return false;
+		} //找到此边不插入
+		p = new Edge(); q = new Edge();
+		p->dest = NodeTable[v2].start;    p->cost = cost;
+		p->netnumber = v3;
+		p->link = NodeTable[v1].adj;
+		NodeTable[v1].adj = p;//链入v1表 
+		q->dest = NodeTable[v1].start;    q->cost = cost;
+		q->netnumber = v3;
+		q->link = NodeTable[v2].adj;
+		NodeTable[v2].adj = q;
+		numEdges++;
+		return true;
+	}
+	return false;
+
+};
---
+++ Insert_Node.cpp
@@ -0,0 +1,20 @@
+#include"insert_node.h"
+bool Graphcnt::insertnode()//插入一个结点 
+{
+	ifstream fin("data1.txt", std::ios::in);
+	char line[1024] = { 0 };
+	numnode = 0;
+	string x = "";
+	while (fin.getline(line, sizeof(line)))
+	{
+		stringstream word(line);
+		word >> x;
+		NodeTable[numnode].start = x;
+		numnode++;
+	}
+
+	fin.clear();
+	fin.close();
+	cout << "Node inserted successfully!!" << endl;
+	return true;
+};
